02bc1a6ebe15a0e8ba6e4a4cf2f2114499706bf5
Eren Sırakaya <poisonweeb@gmail.com>
2020-10-23 21:42:06 +0300

    initial

---
+++ app/src/main/java/com/janfranco/mau_vaultdwellers/activities/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.janfranco.mau_vaultdwellers.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.janfranco.mau_vaultdwellers.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
